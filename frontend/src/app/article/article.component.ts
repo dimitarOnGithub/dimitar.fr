91467cd0cca6a5b45f1691b5e824bf22fe6705e5
Winter <dimitarxivanov@gmail.com>
2026-03-14 11:43:02 +0100

    Adding HTML sanitization

--- frontend/src/app/article/article.component.ts
+++ frontend/src/app/article/article.component.ts
@@ -3,6 +3,7 @@ import {ArticleService} from '@app/article/article.service';
 import {rxResource} from '@angular/core/rxjs-interop';
 import {DatePipe} from '@angular/common';
 import {ArticleNavigation} from '@app/article/components/article-navigation/article-navigation';
+import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-article',
@@ -16,6 +17,7 @@ import {ArticleNavigation} from '@app/article/components/article-navigation/arti
 export class ArticleComponent {
 
   private articleService: ArticleService = inject(ArticleService);
+  private sanitizer = inject(DomSanitizer);
 
   id = input.required<number>();
   articleId = computed(() => Number(this.id()))
@@ -23,4 +25,5 @@ export class ArticleComponent {
     params: () => this.articleId(),
     stream: ({ params }) => this.articleService.getArticleById(params)
   })
+  sanitizedArticleContent = computed(() => this.sanitizer.bypassSecurityTrustHtml(this.article.value()?.content ?? ""))
 }
