de259311e510dc6879ded9bbefab42acab1d4642
Winter <dimitarxivanov@gmail.com>
2026-03-29 14:43:27 +0200

    Flyway setup

---
+++ backend/src/main/resources/db/migration/V1__create_tables.sql
@@ -0,0 +1,19 @@
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) NOT NULL,
+  `published_date` datetime NOT NULL,
+  `content` mediumtext NOT NULL,
+  `is_draft` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `guestbook` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `content` mediumtext NOT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `website` varchar(255) DEFAULT NULL,
+  `approved` tinyint(1) NOT NULL,
+  `ip_address` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ip_address` (`ip_address`)
+);
